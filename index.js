948b93c06b450547508fdf5f04f35bc5be2ea9d2
Yaswanth kumar V <118052077+ghostnumbz@users.noreply.github.com>
2025-07-04 09:55:42 +0000

    added comments

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
-const express = require('express')
-const app = express()
-const bodyParser =require('body-parser')
-let  books =[ 
-    { 
-        id: 1,
-        name:'game of thrones',
-        author:'RR martin',
-        pages: '1000',
-        price: '900',
-        isbn: 123,
+// import dependencies
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+
+// empty array to store books
+let books = [];
+
+//middleware -1
+app.use(bodyParser.json());
+
+//middleware -2
+app.use(function (req, res, next) {
+    if (req.url !== "/favicon.ico") {
+        console.log(`method: ${req.method} url: ${req.url}`);
     }
-]
-app.use(bodyParser.json())
-app.get('/books', function(req,res){
-    return res.json({ books})
-})
+    next();
+});
 
-app.get('/books/:bookId', function (req,res){
-    const id =req.params.bookId
-    const book = books.find((e) => e.id === Number(id))
-    return res.json({book})
-})
-app.post ('/book', function(req,res){
-    const bookFromClient = req.body 
-    books.push(bookFromClient)
-   res.json({ status: "success"})})
+// To get all books
+app.get("/books", function (req, res) {
+    return res.json({ books });
+});
 
-   
-app.delete('/book/:bookId', function(req, res) {
+// To get a book by id
+app.get("/books/:bookId", function (req, res) {
+    const id = req.params.bookId;
+    const book = books.find((e) => e.id === Number(id));
+    return res.json({ book });
+});
+
+// To add a book
+app.post("/book", function (req, res) {
+    const bookFromClient = req.body;
+    books.push(bookFromClient);
+    res.json({ status: "success" });
+});
+
+// To delete a book
+app.delete("/book/:bookId", function (req, res) {
     const bookIDToDelete = req.params.bookId;
     books = books.filter((e) => e.id !== Number(bookIDToDelete));
     return res.json({ status: "deleted" });
 });
 
-
-app.listen(8000, ()=>{
-    console.log ("server started ")
-})
+// start server
+app.listen(8000, () => {
+    console.log("server started ");
+});
